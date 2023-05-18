ad9c25ef24037e44600fb7874577f4078c46b02d
rockreyad <rockreyad@gmail.com>
2023-05-18 17:37:55 +0600

    new bar added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  important: true,
+  content: ['./**/*.{liquid,json}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
