6530c1089ca938fd9f192ab8667326f07da4ca38
rockreyad <rockreyad@gmail.com>
2023-05-24 10:49:16 +0600

    custom section created

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: ["./sections/*.liquid","./templates/*.liquid","./snippets/*.liquid"],
+  theme: {
+    extend: {
+
+    },
+  },
+  plugins: [],
+}
+
